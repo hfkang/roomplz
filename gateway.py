333cf04067681298ee599e40495327c0dc5d6385
Francis <francis.kang@mail.utoronto.ca>
2015-11-28 16:00:30 -0500

    Subdomain cookies

--- gateway.py
+++ gateway.py
@@ -171,7 +171,7 @@ def application (environ, start_response):
 
         if 'pswd' in req.POST and req.POST['pswd'] == 'potato salad':
             text = 'authenticated. return to main page'
-            resp.set_cookie("auth",value="potato",overwrite=True,httponly=True,max_age=2000000)
+            resp.set_cookie("auth",value="potato",domain='toastedsesa.me',overwrite=True,httponly=True,max_age=2000000)
         else:
             text = login()
     resp.text = text
