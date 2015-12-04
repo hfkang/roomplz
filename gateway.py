72841f707a006a9f6b8850ee6b30ff072cdd3d7c
Francis <francis.kang@mail.utoronto.ca>
2015-12-03 20:17:31 -0500

    Removed async nonsenes

--- gateway.py
+++ gateway.py
@@ -58,7 +58,7 @@ def construct_page(q_s):
                 <meta name="msapplication-TileColor" content="#da532c">
                 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
                 <meta name="theme-color" content="#ffffff"> 
-                <script type="text/javascript" src="/mainanalytics.js" async></script> 
+                <script type="text/javascript" src="/mainanalytics.js" ></script> 
                 </head>"""
     
     body = """<body> 
