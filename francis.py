a6829d954341cdaae54378568eebbf9e9a490bce
Francis <francis.kang@mail.utoronto.ca>
2015-12-12 19:52:57 -0500

    Tried to change the way the css is handled to no avail

--- francis.py
+++ francis.py
@@ -2,14 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/francis')
+def francis():
+    return "Hi i'm francis"
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return 'You want path: %s' % path
 
-@app.route('/francis')
-def francis():
-    return 'Franck'
 
 
 
